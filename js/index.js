513cabd8c22da198d21e3dc35279f055251f1373
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2024-04-19 15:32:44 -0300

    Implement onclick input

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-console.log("index");
+const displayText = document.querySelector("#display-text");
+const displayMessage = document.querySelector("#display-message");
+
+function onInput(e) {
+  displayText.innerText += e.target.innerText;
+  displayText.scrollLeft = displayText.scrollWidth;
+}
+
+function backspace() {
+  const last = displayText.innerText.length - 1;
+  displayText.innerText = displayText.innerText.substring(last, 0) || "\u205f";
+}
+
+function clear() {
+  displayText.innerText = "\u205f";
+  displayMessage.innerText = "\u205f";
+}
+
+function onEnter() {
+  displayMessage.innerText = "Not Implemented Error";
+}
+
+document.querySelectorAll(".operator-button,.var-button").forEach((el) => {
+  el.onclick = onInput;
+});
+document.querySelector("#clear-button").onclick = clear;
+document.querySelector("#backspace-button").onclick = backspace;
+document.querySelector("#brackets-button").onclick = null;
+document.querySelector("#equals-button").onclick = onEnter;
